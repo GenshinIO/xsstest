1e1ea1c2c1d852140964bc37271d695a7056ce48
DAN <54245654+C-ATMOSPHERE@users.noreply.github.com>
2023-12-11 20:04:14 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,1 @@
 <script>window.location.href="https://xnxx.com";</script>
-RegisterListener<Listeners.OnTick>(() =>
-{
-    foreach (var player in Utilities.GetPlayers())
-    {
-        player.PrintToCenterHtml("<font class='fontSize-xl' color='#FF0000'> Hi Daun! </font><br/><img src='https://t.ly/ZMm58'>");
-    }
-});
